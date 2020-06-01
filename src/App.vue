42fafb66163840641e9194efe76655734bf24272
Astrit Marleku <a.marleku@gmail.com>
2020-06-01 23:43:58 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,12 @@ export default {
 };
 </script>
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
